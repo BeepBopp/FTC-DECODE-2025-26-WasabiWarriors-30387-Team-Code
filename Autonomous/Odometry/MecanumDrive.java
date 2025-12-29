80ef51938b56a764592af19891e7f054569e8e08
BeepBopp <115367530+BeepBopp@users.noreply.github.com>
2025-12-28 22:58:13 -0500

    Increase velocity and acceleration parameters for MecanumDrive

--- Autonomous/Odometry/MecanumDrive.java
+++ Autonomous/Odometry/MecanumDrive.java
@@ -73,13 +73,13 @@ public final class MecanumDrive {
         public double kA = 0.000001;
 
         // path profile parameters (in inches)
-        public double maxWheelVel = 50;
-        public double minProfileAccel = -30;
-        public double maxProfileAccel = 50;
+        public double maxWheelVel = 100;  // 50 * 2 = 100
+        public double minProfileAccel = -60;  // -30 * 2 = -60
+        public double maxProfileAccel = 100;  // 50 * 2 = 100
 
         // turn profile parameters (in radians)
-        public double maxAngVel = Math.PI; // shared with path
-        public double maxAngAccel = Math.PI;
+        public double maxAngVel = Math.PI * 2;  // shared with path
+        public double maxAngAccel = Math.PI * 2;
 
         // path controller gains
         public double axialGain = 0.9;
@@ -453,14 +453,14 @@ public final class MecanumDrive {
     public PoseVelocity2d updatePoseEstimate() {
         PoseVelocity2d vel = localizer.update();
         poseHistory.add(localizer.getPose());
-        
+
         while (poseHistory.size() > 100) {
             poseHistory.removeFirst();
         }
 
         estimatedPoseWriter.write(new PoseMessage(localizer.getPose()));
-        
-        
+
+
         return vel;
     }
 
